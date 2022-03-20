334882adbbc4f9a2105a82eaa9681d933fb01cd8
Marat <zinatulinmarat@gmail.com>
2022-03-20 17:28:00 +0500

    fff

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { AdminMenuModule } from './modules/admin-menu/admin-menu.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
     AuthModule,
-    ConfigModule.forRoot()
+    ConfigModule.forRoot(),
+    AdminMenuModule
   ],
   controllers: [AppController],
   providers: [AppService],
